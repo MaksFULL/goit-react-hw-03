1c4d7d81c1f008050d3b04f4f3892634ae5903ab
MaksFULL <169079873+MaksFULL@users.noreply.github.com>
2024-12-22 13:50:01 +0200

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react';
 import './App.css'
+import ContactForm from './components/ContactForm/ContactForm';
+import ContactList from './components/ContactList/ContactList';
+import SearchBox from './components/SearchBox/SearchBox';
+import { nanoid } from 'nanoid';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const initContacts = () => {
+    if (localStorage.getItem("contacts") === null) {
+      return [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ];
+    }
+    else { return JSON.parse(localStorage.getItem("contacts")) };
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  const [contactList, setContactList] = useState(initContacts);
+  useEffect(() => localStorage.setItem("contacts", JSON.stringify(contactList)), [contactList]);
 
-export default App
+  const [search, setSearch] = useState("");
+  const filteredContacts = contactList.filter(contact => contact.name.toLowerCase().includes(search));
+
+  const handleSearch = (e) => setSearch(e.target.value.toLowerCase());
+  const handleSubmit = (values, actions) => {
+    const data = { ...values, id: nanoid(4) };
+    actions.resetForm();
+    setContactList((prev) => ([...prev, data]))
+  };
+  const handleDelete = (id) => {
+    setContactList(() => contactList.filter(item => item.id !== id));
+  };
+
+  return <div>
+    <h1>Phone<span>book</span></h1>
+    <ContactForm handleSubmit={handleSubmit} />
+    <SearchBox handleSearch={handleSearch} search={search} />
+    <ContactList contactList={filteredContacts} handleDelete={handleDelete} />
+  </div>
+};
+
+export default App;
