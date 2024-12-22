aa7ddff12124dd9496185aa7c97ee2468edb09ba
MaksFULL <169079873+MaksFULL@users.noreply.github.com>
2024-12-22 14:10:54 +0200

    contactList

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,10 @@
+import Contact from "../Contact/Contact";
+import s from "./ContactList.module.css";
+
+const ContactList = ({ contactList, handleDelete }) => {
+    return <ul className={s.listbox}>
+        {contactList.map(contact => <Contact key={contact.id} contact={contact} handDelete={handleDelete} />)}
+    </ul>
+};
+
+export default ContactList;
